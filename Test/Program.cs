1df1596845f964988e05764f8847d36607a385b5
大石头 <nnhy@vip.qq.com>
2017-12-26 20:42:27 +0800

    让GC闭嘴，然后性能翻了3倍

--- Test/Program.cs
+++ Test/Program.cs
@@ -63,29 +63,20 @@ namespace Test
         {
             //Console.ReadKey();
 
+            // GC闭嘴
+            GC.TryStartNoGCRegion(10_000_000);
+
             var bk = new Block(256, 16_000_000_000);
             var hp = new Heap(bk);
 
-            var list = new List<Block>();
-            var count = 1_000_000;
+            var count = 10_000_000;
+            var list = new List<Block>(count);
             var sw = Stopwatch.StartNew();
             for (var i = 0; i < count; i++)
             {
-                // 一般几率申请，一半释放
-                if (list.Count == 0 || Rand.Next(2) == 0)
-                {
-                    // 申请随机大小
-                    bk = hp.Alloc(Rand.Next(1, 10_000_000));
-                    list.Add(bk);
-                }
-                else
-                {
-                    // 随机释放一块
-                    var idx = Rand.Next(0, list.Count);
-                    bk = list[idx];
-                    list.RemoveAt(idx);
-                    hp.Free(bk);
-                }
+                // 申请随机大小
+                bk = hp.Alloc(16);
+                list.Add(bk);
             }
             sw.Stop();
             // 结果
@@ -95,15 +86,18 @@ namespace Test
             }
             Console.WriteLine("耗时：{0:n0}ms 速度 {1:n0}ops", sw.ElapsedMilliseconds, count * 1000L / sw.ElapsedMilliseconds);
 
-            Console.WriteLine();
+            sw.Reset(); sw.Restart();
             foreach (var item in list)
             {
                 hp.Free(item);
             }
+            sw.Stop();
+            // 结果
             foreach (var pi in hp.GetType().GetProperties())
             {
                 Console.WriteLine("{0}\t{1:n0}", pi.Name, hp.GetValue(pi));
             }
+            Console.WriteLine("耗时：{0:n0}ms 速度 {1:n0}ops", sw.ElapsedMilliseconds, count * 1000L / sw.ElapsedMilliseconds);
 
             //var db = new Database("test.db");
             //Console.WriteLine(db.Version);
